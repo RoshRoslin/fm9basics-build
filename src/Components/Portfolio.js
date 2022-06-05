530c3be4ff337e667a39738f16f143fdee16694e
RoshRoslin <RoshRoslin@gmail.com>
2022-06-05 14:56:10 -0700

    updated url link for images

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,7 +13,9 @@ class Portfolio extends Component {
       return (
         <div key={id++} className='columns portfolio-item'>
           <div className='item-wrap'>
-            <Zmage alt={projects.title} src={projectImage} />
+            <a href={projects.url} target='_blank' rel='noopener noreferrer'>
+              <Zmage alt={projects.title} src={projectImage} />
+            </a>
             <div style={{ textAlign: 'center' }}>
               <a href={projects.url} target='_blank' rel='noopener noreferrer'>
                 {projects.title}
